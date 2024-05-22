f62f20120e1d151b5b387abebd66087aa8758bfd
Adam Huss <adamhuss13@gmail.com>
2024-05-21 20:47:23 -0500

    added about me filler

---
+++ components/AboutMe.jsx
@@ -0,0 +1,13 @@
+const About= () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam a
+      laboriosam quia neque veniam delectus natus, in aliquam itaque? Omnis
+      eaque officia nobis nostrum eveniet perspiciatis debitis provident fugit
+      reprehenderit.
+    </p>
+  </section>
+);
+
+export default About;
