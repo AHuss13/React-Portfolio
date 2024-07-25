1db390e6365f844671cbd061fb3c5241d64c6569
AHuss13 <Adamhuss13@gmail.com>
2024-07-25 18:46:51 -0500

    added Under Construction banner to top of page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Navigation";
 import About from "./components/AboutMe";
-import Project from "./components/Project";
+import Project from "./pages/MyWork";
 import Footer from "./components/Footer";
+import UnderConstruction from "./components/Disclaimer";
 
 const App = () => (
   <div className="App">
     <Header />
     <Nav />
+    <UnderConstruction />
     <About />
     <Project />
     <Footer />
