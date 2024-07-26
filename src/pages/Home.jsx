3b8340272f0f68127e16364292da9ac5b3639f1d
AHuss13 <Adamhuss13@gmail.com>
2024-07-25 20:08:15 -0500

    created files to start home page and separate CG work page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+import UnderConstruction from "../components/Disclaimer";
+import Header from "../components/Header";
+import Nav from "../components/Navigation";
+import Footer from "../components/Footer";
